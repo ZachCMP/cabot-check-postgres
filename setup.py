9709812d9a7ed815e5ceb0fe08eeff84a099a2e2
Zach Parks <zparks@zatohealth.com>
2019-04-25 16:58:10 -0400

    Change metadata

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 
 from setuptools import find_packages, setup
 
-setup(name='cabot_check_network',
+setup(name='cabot_check_postgres',
       version='0.1.0',
-      description='A network check plugin for Cabot by Arachnys',
-      author='Arachnys',
-      author_email='techteam@arachnys.com',
-      url='http://cabotapp.com',
+      description='A postgres check plugin for Cabot by Arachnys',
+      author='ZachCMP',
+      author_email='zach.cm.parks@gmail.com',
+      url='https://github.com/ZachCMP/cabot-check-postgres',
       packages=find_packages(),
     )
